0d252c2e5ac3e74253c89114a9e5e791e23e375a
kamil <you@example.com>
2022-07-02 11:11:45 +0200

    add dataCheck helpers

---
+++ utils/dataCheck.ts
@@ -0,0 +1,39 @@
+import {ValidationError} from "./errors";
+
+export const isNull = (variable: any, name?: string, errorName?: string): void => {
+    if (variable === null) {
+        throw new ValidationError(errorName ? errorName :`${name ? name : Object.keys({variable})[0]} does not exist`);
+    }
+}
+
+export const isNotNull = (variable: any, name?: string,errorName?: string): void => {
+    if (variable !== null) {
+        throw new ValidationError(errorName ? errorName :`${name ? name : Object.keys({variable})[0]} does exist`);
+    }
+}
+
+export const isBetween = (variable: any, first: number, second: number, name?: string, errorName?: string) : void => {
+
+    let isString = false
+    if (typeof(variable) === 'string') {
+        isString = true
+    }
+
+    if (first > second) {
+        if (variable.length > first || variable.length < second) {
+            throw new ValidationError(errorName ? errorName : `${name ? name : Object.keys({variable})[0]} should be beetwen ${second} and ${first} ${isString ? 'characters' : 'value'}.`);
+        }
+
+    } else {
+        if  (variable.length < first || variable.length > second) {
+            throw new ValidationError(errorName ? errorName :`${name ? name : Object.keys({variable})[0]} should be beetwen ${first} and ${second} ${isString ? 'characters' : 'value'}.`);
+        }
+    }
+}
+
+export const exists = (variable: any, name?: string,errorName?: string): void => {
+
+    if (!variable) {
+        throw new ValidationError(errorName ? errorName :`${name ? name : Object.keys({variable})[0]} does not exist`);
+    }
+}
