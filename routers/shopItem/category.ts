59e5c8935c7a03c8fa41e0bacd766650b18610be
kamil <you@example.com>
2022-07-09 18:09:16 +0200

    add category routes authentication

--- routers/shopItem/category.ts
+++ routers/shopItem/category.ts
@@ -1,9 +1,8 @@
 import {Router} from "express";
 import {CategoryRecord} from "../../records";
-import {CategoryEntity, CreateCategoryReq, SetCategoryForCategoryReq} from "../../types";
-import {exists, isBetweenEqual, isNotNull, isNull} from "../../utils/dataCheck";
+import {exists, isNull} from "../../utils/dataCheck";
+
 export const categoryRouter = Router();
-const errorInfoName = 'category'
 
 categoryRouter
 
@@ -15,55 +14,13 @@ categoryRouter
         })
     })
     .get('/one/:id', async (req, res) => {
-        exists(req.params.id, 'id param')
-        const category = await CategoryRecord.getOne(req.params.id);
+
+        const {id} = req.params;
+        exists(id, 'id param')
+        const category = await CategoryRecord.getOne(id);
         isNull(category, null,'category does not exists')
 
         res.json({
             category,
         })
     })
-
-    .post('/', async (req, res) => {
-        const { body } : {
-            body: SetCategoryForCategoryReq
-        } = req
-
-        exists(body.name, 'name')
-        const category = await CategoryRecord.getOneByName(body.name);
-        isNotNull(category, null,'category with this name already exists')
-
-        const newCategory = new CategoryRecord(req.body as CreateCategoryReq);
-        await newCategory.insert();
-
-        res.json(newCategory as CategoryEntity);
-    })
-
-    .patch('/:categoryId', async (req, res) => {
-        const { body } : {
-            body: SetCategoryForCategoryReq
-        } = req
-
-        exists(req.params.categoryId, 'category Id')
-        const category = await CategoryRecord.getOne(req.params.categoryId);
-        isNull(category, null,'No category found for this ID.')
-
-        exists(body.name, 'name')
-        const categoryName = await CategoryRecord.getOneByName(body.name);
-        isNotNull(categoryName, null,'category with this name already exists')
-        isBetweenEqual(body.name.length, 3, 20, errorInfoName)
-
-        category.name = body.name;
-        await category.update();
-
-        res.json(category)
-    })
-
-    .delete('/:categoryId', async (req, res) => {
-        exists(req.params.categoryId, 'categoryId')
-        const category = await CategoryRecord.getOne(req.params.categoryId);
-        isNull(category, null,'No category found for this ID.')
-
-        await category.delete();
-    res.json({message: "Category deleted successfully."})
-});
--- types/category/category.ts
+++ types/category/category.ts
@@ -5,3 +5,16 @@ export type CreateCategoryReq = Omit<CategoryEntity, 'id'>;
 export interface SetCategoryForCategoryReq {
     name: string;
 }
+
+export interface UpdateCategoryForCategoryReq {
+    name: string;
+    id: string;
+}
+
+export interface GetOneCategoryReq {
+    id: string;
+}
+
+export interface DeleteOneCategoryReq {
+    id: string;
+}
