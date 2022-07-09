6af55babe6238d315f1e2f358c03667409be08ec
kamil <you@example.com>
2022-07-09 10:59:02 +0200

    added new errors

--- utils/errors.ts
+++ utils/errors.ts
@@ -2,6 +2,10 @@ import {NextFunction, Request, Response} from "express";
 
 export class ValidationError extends Error {}
 export class NotFoundError extends Error {}
+export class AuthInvalidError extends Error {}
+export class TokenError extends Error {}
+export class InvalidTokenError extends Error {}
+
 export const handleError = (err: Error, req: Request, res: Response, next: NextFunction) : void=> {
 
 
@@ -10,8 +14,35 @@ export const handleError = (err: Error, req: Request, res: Response, next: NextF
     if (err instanceof NotFoundError) {
         res
             .status(404)
-            .render('error', {
-                message: 'Nie można znaleźć elementu o danym ID.',
+            .json( {
+                message: 'Not found',
+            });
+        return;
+    }
+
+    if (err instanceof TokenError) {
+        res
+            .status(401)
+            .json( {
+                message: 'no token',
+            });
+        return;
+    }
+
+    if (err instanceof AuthInvalidError) {
+        res
+            .status(403)
+            .json( {
+                message: 'no access',
+            });
+        return;
+    }
+
+    if (err instanceof InvalidTokenError) {
+        res
+            .status(403)
+            .json( {
+                message: 'InvalidTokenError',
             });
         return;
     }
