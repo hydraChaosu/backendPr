fc2bbd2022c47e0ad1034303b26c81013478eb1e
kamil <you@example.com>
2022-08-27 13:26:30 +0200

    removed unnecessary elements

--- types/user/userAuth.ts
+++ types/user/userAuth.ts
@@ -1,18 +1,6 @@
 import { Request } from "express";
 import { UserTokenResponse } from "./user";
-import { Session } from "express-session";
 
 export interface UserAuthReq extends Request {
   user: UserTokenResponse;
 }
-
-export interface SessionData {
-  user: { [key: string]: any };
-}
-
-export type AuthRequest = Request & {
-  session?: SessionWithUser;
-  auth?: { user: string; permission_id: number };
-};
-
-export type SessionWithUser = Session & { user: string | {} };
