ddaa80c78d87d4be20500f0753007c760de4f996
kamil <you@example.com>
2022-09-08 16:09:24 +0200

    make cors working

--- index.ts
+++ index.ts
@@ -20,24 +20,10 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const app = express();
 
-// app.use(
-//   expressWinston.logger({
-//     transports: [new winston.transports.Console()],
-//     format: winston.format.combine(
-//       winston.format.colorize(),
-//       winston.format.json()
-//     ),
-//     meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-//     msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
-//     expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
-//     colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
-//     // ignoreRoute: function (req, res) { return false; } // optional: allows to skip some log messages based on request and/or response
-//   })
-// );
-
 app.use(
   cors({
     origin: "http://localhost:3000",
+    credentials: true,
   })
 );
 app.use(express.json()); // Content-type: application/json
